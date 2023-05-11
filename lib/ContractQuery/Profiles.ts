23f030ce35bac5ffd3184a1ef267f4239cc5e859
mrisholukamba <abdulrazzaqlukamba@gmail.com>
2023-05-11 04:09:32 +0300

    Contract

---
+++ lib/ContractQuery/Profiles.ts
@@ -0,0 +1,16 @@
+import { ApplicantProfile,IssuerProfile,Categories,Chains,AccountId} from "../contractTypes/ordumTypes";
+import { onSignCertificate, certificateCache } from "../ContractFns/createProfile";
+
+// Certificate handling
+
+
+// Applicant Profile Query
+
+
+// Issuer Profile Query
+
+
+// All Applicants
+
+
+// All Issuers

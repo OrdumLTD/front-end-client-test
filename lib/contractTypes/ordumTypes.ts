f8fe7e52d8c6a72808b9e8e13d87e0508e7d9383
mrisholukamba <abdulrazzaqlukamba@gmail.com>
2023-06-15 17:37:23 +0300

    Added types

--- lib/contractTypes/ordumTypes.ts
+++ lib/contractTypes/ordumTypes.ts
@@ -6,15 +6,14 @@ export enum Categories {
 	mediaArt = 'MediaArt'
 }
 
-export type IssuerProfile = {
-	name: string,
-	id: (number | string | BN),
-	chain: Chains,
-	isActive: boolean,
-	registrationDate: (number | string | BN),
-	categories: Array<Categories>,
-	description: string,
-	applications: Array<(number | string | BN)> | null
+export enum UserRole {
+	applicant = 'Applicant',
+	foundation = 'Foundation'
+}
+
+export enum MemberRole {
+	admin = 'Admin',
+	regular = 'Regular'
 }
 
 export enum Chains {
@@ -22,23 +21,6 @@ export enum Chains {
 	offChain = 'OffChain'
 }
 
-export type AccountId = string | number[]
-
-export type ApplicantProfile = {
-	name: string,
-	teamSize: (number | string | BN),
-	accountId: AccountId,
-	description: string,
-	registeredTime: (number | string | BN),
-	applications: (number | string | BN) | null,
-	categories: Array<Categories>
-}
-
-export type KeyManagement = {
-	admin: AccountId,
-	keyPointer: AccountId,
-	allowedKeys: Array<AccountId>
-}
 
 export enum LangError {
 	couldNotReadInput = 'CouldNotReadInput'
@@ -58,4 +40,42 @@ export enum KeyAction {
 	add = 'ADD',
 	remove = 'REMOVE',
 	changeAdmin = 'ChangeAdmin'
+}
+
+
+export type AccountId = string | number[]
+
+export type IssuerProfile = {
+	name: string,
+	id: (number | string | BN),
+	chain: Chains,
+	isActive: boolean,
+	registrationDate: (number | string | BN),
+	categories: Array<Categories>,
+	description: string,
+	mission: string,
+	members: Array<[AccountId,MemberRole]>|null,
+	applications: Array<(number | string | BN)> | null
+	role: UserRole
+}
+
+
+
+export type ApplicantProfile = {
+	name: string,
+	teamSize: (number | string | BN),
+	accountId: AccountId,
+	description: string,
+	members: Array<[AccountId,MemberRole]>|null,
+	refTeam: (number | string | BN) | null,
+	registeredTime: (number | string | BN),
+	applications: (number | string | BN) | null,
+	categories: Array<Categories>,
+	role: UserRole
+}
+
+export type KeyManagement = {
+	admin: AccountId,
+	keyPointer: AccountId,
+	allowedKeys: Array<AccountId>
 }
